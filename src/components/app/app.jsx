cc299465a7fb087e86cc4dd62e5136573c182975
Mistesq <mistesqr@gmail.com>
2020-04-14 15:07:11 +0300

    Render film

---
+++ src/components/app/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Main from '../main/main.jsx';
+
+const App = ({promoFilm}) => {
+  return <Main promoFilm={promoFilm} />;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import App from './components/app/app.jsx';
+
+const PromoFilm = {
+  filmName: `The Grand Budapest Hotel`,
+  filmGenre: `Drama`,
+  filmYear: 2014
+};
+
+ReactDOM.render(
+    <App promoFilm={PromoFilm}/>,
+    document.querySelector(`#root`)
+);
